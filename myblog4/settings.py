49ec6f82ce04815a9f230666b3dd2cb8b6852a75
phamtrunghieuht <kevinphamht@gmail.com>
2020-06-03 07:01:12 +0700

    make it better

--- myblog4/settings.py
+++ myblog4/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 import django_heroku
 django_heroku.settings(locals())
